5293d6d539eefbebce142fad0ccd2e361b987934
sumit celly <scelly@securevideo.com>
2025-03-12 11:56:52 -0600

    dfd

--- code/CreateTicketApi/obj/Debug/net8.0/CreateTicketApi.AssemblyInfo.cs
+++ code/CreateTicketApi/obj/Debug/net8.0/CreateTicketApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CreateTicketApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9045fcd4a47c92d09ccc5612cbadb5d0f76478ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c963e9944d174dbbc279b0710e7ff2926a59e642")]
 [assembly: System.Reflection.AssemblyProductAttribute("CreateTicketApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CreateTicketApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
