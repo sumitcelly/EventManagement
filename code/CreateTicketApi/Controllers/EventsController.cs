044f051433dff2d112e22c8be172995c580ebe95
sumitcelly <sumitcelly@hotmail.com>
2025-03-13 16:10:49 -0600

    testnew

--- code/CreateTicketApi/Controllers/EventsController.cs
+++ code/CreateTicketApi/Controllers/EventsController.cs
@@ -34,6 +34,7 @@ public class EventsController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
+        
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
